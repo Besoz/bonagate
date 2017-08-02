ffe4e4cb3addd42e026519d7872a1cb7c5a3667f
Hadeer Fawzy <hadeer.fawzy@accorpa.com>
2017-08-02 14:10:22 +0200

    detail category page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,6 +10,7 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
+
 # user-section stylesheets
 Rails.application.config.assets.precompile += %w( realestate-user/all.css )
 Rails.application.config.assets.precompile += %w( realestate-user/en.css )
