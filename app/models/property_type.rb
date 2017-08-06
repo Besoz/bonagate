259490cdb05128ba5f332c9f1db422aa4438f1bf
NehalMorsi <nehal.morsi@gmail.com>
2017-08-06 15:18:27 +0200

    update property type form
    change property detail submission methodology, by using detail.ids indstead of hard coded assiging of details  in the controller.

--- app/models/property_type.rb
+++ app/models/property_type.rb
@@ -13,11 +13,11 @@ class PropertyType < ActiveRecord::Base
   validates :property_type_states, :presence => true
 
 
-  def set_property_details property_details_arr
-    property_detail_ids = property_details_arr.map { |x| x[:id] }
-    property_details = PropertyDetail.where(id: property_detail_ids)
-    self.property_details = property_details
-  end
+  # def set_property_details property_details_arr
+  #   property_detail_ids = property_details_arr.map { |x| x[:id] }
+  #   property_details = PropertyDetail.where(id: property_detail_ids)
+  #   self.property_details = property_details
+  # end
 
   def self.get_affected_with_property_detail detail_id
     PropertyType.joins(:property_type_details).where(property_type_details: { property_detail_id: detail_id })
--- app/views/property_types/_property_type.json.jbuilder
+++ app/views/property_types/_property_type.json.jbuilder
@@ -1,4 +1,5 @@
 json.extract! property_type, :id, :code, :name, :state, :created_at, :updated_at, :state
-json.property_details_attributes property_type.property_details, partial: "property_details/property_detail_min",as: :property_detail
+# json.property_details_attributes property_type.property_details, partial: "property_details/property_detail_min",as: :property_detail
+json.property_detail_ids property_type.property_details.map {|detail| detail.id } 
 json.url property_type_url(property_type, format: :json)
 json.property_state_ids property_type.property_states.map {|state| state.id } 
