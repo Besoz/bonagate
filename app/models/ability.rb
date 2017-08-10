Besoz/bonagate

commit d4521ed63bfc1373527e79d34e03458b3987dc44

Add allow non-users to view published properties

app/models/ability.rb
@@ -5,6 +5,10 @@ class Ability
     alias_action :create, :read, :update, :destroy, :to => :crud
     alias_action :index, :create, :read, :update, :destroy, :to => :crud_all
 
+    can [:index, :read, :search], Property, Property.published do |property|
+          property.publish
+    end
+    
     user ||= User.new # guest user (not logged in)
     if user.admin?
       can :manage, :all
@@ -34,9 +38,6 @@ class Ability
       can :upload_image, Property, :company_id => user.company_user.company_id # to be changed company_user.company_id
       else
         can [:change_password, :user_profile], User
-        can [:index, :read, :search], Property, Property.published do |property|
-          property.publish
-        end
     end
     #
     # The first argument to `can` is the action you are giving the user

commit b07fbc978eadcb5da9a58cfb4b41b950740a175d

Fix bug caused by d4521ed commit

app/models/ability.rb
@@ -4,10 +4,6 @@ class Ability
   def initialize(user)
     alias_action :create, :read, :update, :destroy, :to => :crud
     alias_action :index, :create, :read, :update, :destroy, :to => :crud_all
-
-    can [:index, :read, :search], Property, Property.published do |property|
-          property.publish
-    end
     
     user ||= User.new # guest user (not logged in)
     if user.admin?
@@ -38,6 +34,12 @@ class Ability
       can :upload_image, Property, :company_id => user.company_user.company_id # to be changed company_user.company_id
       else
         can [:change_password, :user_profile], User
+    end
+
+    if not (user.admin? or user.company_user?)
+      can [:index, :read, :search], Property, Property.published do |property|
+          property.publish
+      end
     end
     #
     # The first argument to `can` is the action you are giving the user