1f0dcb9b954bb55c5c0a10eca5008140bced6491
Mohammed Abuissa <mohammed.abuissa@accorpa.com>
2017-08-24 21:42:01 +0200

    Hide pagination and display loading indicator while loading

--- app/assets/javascripts/app.realestate/controllers/favorites.controller.js
+++ app/assets/javascripts/app.realestate/controllers/favorites.controller.js
@@ -5,19 +5,24 @@
   .module('app')
   .controller('FavoritesController', FavoritesController);
 
+  FavoritesController.$inject = ['$scope', '$rootScope', '$translate', '$http', '$location', 'decoratorService', 'GeneralDataServices', 'toaster'];
+
   function FavoritesController($scope, $rootScope, $translate, $http, $location, decoratorService, GeneralDataServices, toaster) {
     var vm = this;
-    window.onpopstate = loadDataFromServer;
+
+    vm.dataLoaded = false;
+
+    activate();
     
     var PaginationInfo = function(totalItems, currentPage, pageChangedEvent){
       this.totalItems = totalItems;
       this.currentPage = currentPage;
       this.pageChanged = pageChangedEvent
       this.maxSize = 5;
-      
     }
 
     function getFavorites(pageNumber){
+      vm.dataLoaded = false;
       pageNumber = pageNumber || 1
       return $http.get('/user_profile/favorites.json?page=' + pageNumber)
                 .then((response) => {
@@ -25,21 +30,24 @@
                                       var paginationInfo = response.data.pagination_info;
                                       vm.paginationInfo = new PaginationInfo(paginationInfo.totalItems, 
                                                                               paginationInfo.currentPage,
-                                                                              vm.pageChangedEvent);
+                                                                              pageChangedEvent);
+                                      vm.dataLoaded = true;
                                   });
-
     }
 
     function loadDataFromServer(){
       var currentPage = parseInt($location.search().page)
       getFavorites(currentPage);
-    } 
+    }     
 
-    vm.pageChangedEvent = function() {
+    function activate(){
+      window.onpopstate = loadDataFromServer;
+      getFavorites(parseInt($location.search().page));
+    }
+
+    function pageChangedEvent() {
       getFavorites(vm.paginationInfo.currentPage);
       $location.search('page', vm.paginationInfo.currentPage);
-    };
-
-    getFavorites(parseInt($location.search().page));
+    }
   }
 })();
