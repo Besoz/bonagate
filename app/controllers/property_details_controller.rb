c226b38aa34c27a2a63576e9de3746141130328c
Mohammed Abuissa <mohammed.abuissa@accorpa.com>
2017-08-26 21:54:08 +0200

    Add state to property details

--- app/controllers/property_details_controller.rb
+++ app/controllers/property_details_controller.rb
@@ -120,7 +120,7 @@ class PropertyDetailsController < ApplicationController
   # Never trust parameters from the scary internet, only allow the white list through.
   def property_detail_params
     params.require(:property_detail).permit(:id, :code, :name, :name_en, :name_ar, :value_type, :mandatory, 
-    :details_ids, :property_detail_category_id, :value_options => [], 
+    :state, :details_ids, :property_detail_category_id, :value_options => [], 
     property_detail_value_options_attributes: [:id, :name_en, :name_ar, :_destroy])
   end
 
